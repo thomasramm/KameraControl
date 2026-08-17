8de82588f5ec913614c41ab41da6ceaee690c540
Thomas Ramm <tomyramm@gmail.com>
2026-08-17 21:15:15 +0200

    Settings Icon Vorschau

--- KameraSteuerungDeLuxe/Core/Converter.cs
+++ KameraSteuerungDeLuxe/Core/Converter.cs
@@ -4,6 +4,17 @@ using System.Windows.Data;
 
 namespace KameraSteuerungDeLuxe.Core
 {
+    public class ImagePathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"images/{value}.png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
     public class RemoveExtensionConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
